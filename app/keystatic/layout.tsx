88e2dbed70c67d36c8e50c8c511b9c6a6c6be5c2
Shaswat Raj <shaswatraj3@gmail.com>
2026-09-19 04:14:29 +0530

    fix: keep Keystatic server-only on non-Pages deployments

--- app/keystatic/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { notFound } from "next/navigation";
-import KeystaticApp from "./keystatic";
-import { showAdminUI } from "../../keystatic.config";
-
-export default function KeystaticLayout({ children }: { children: React.ReactNode }) {
-  if (!showAdminUI) notFound();
-  return <><KeystaticApp />{children}</>;
-}
